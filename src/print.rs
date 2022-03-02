84c11fbfc7dacf973107daded5fe719c630d92cc
coderasha <ashadipta@gmail.com>
2022-03-02 16:40:00 +0530

    Placeholder traits

--- src/print.rs
+++ src/print.rs
@@ -3,8 +3,17 @@ pub fn run(){
     println!("I am a good boy ");
     println!("Number= {} ",1);
     println!("{} and {} are good boys ","Ram","Shyam");
+    //Positional Arguements
     println!("Among {0} and {1} , {0} is a better boy ","Ram","Shyam");
-    println!("Among {name} and {activity}  ",name="Ram",activity = "Play");
+    //maned Agruements
+    println!(
+        "Among {name} and {activity}  ",name="Ram",activity = "Play"
+    );
+    //Placeholeder traits
+    println!(
+        "Binary: {:b} and Hex: {:x} Octal: {:o}",10,10,10 
+    );
+
 
 
 }
