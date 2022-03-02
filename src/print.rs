9888f63b414e62c274d66b4322049b46bcc32a7d
coderasha <ashadipta@gmail.com>
2022-03-02 16:28:45 +0530

    Rust coding

---
+++ src/print.rs
@@ -0,0 +1,10 @@
+pub fn run(){
+
+    println!("I am a good boy ");
+    println!("Number= {} ",1);
+    println!("{} and {} are good boys ","Ram","Shyam");
+    println!("Among {0} and {1} , {0} is a better boy ","Ram","Shyam");
+    println!("Among {name} and {activity}  ",name="Ram",activity = "Play");
+
+
+}
