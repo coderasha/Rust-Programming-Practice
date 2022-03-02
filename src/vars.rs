ae6b0a8a3f7f8875cea66895d9c2c33a5d53105a
coderasha <ashadipta@gmail.com>
2022-03-02 16:59:38 +0530

    Rust variables

--- src/vars.rs
+++ src/vars.rs
@@ -1,6 +1,13 @@
 pub fn varun() {
 
     let name= "Brad" ;
-    let age = 37;
+    let mut age = 37;
     println!("My name is {} and age is {}",name,age);
+    age = 38;
+    println!("My name is {} and age is {}",name,age);
+
+    const ID: i32 =001;
+    println!("ID:{}",ID);
+
+    
 }
