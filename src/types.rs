4be57a62129489db9946b3788b75b01a3f881f37
coderasha <ashadipta@gmail.com>
2022-03-03 09:44:27 +0530

    Rust Types

--- src/types.rs
+++ src/types.rs
@@ -3,6 +3,7 @@ pub fn typrun() {
     let x = 1;
     let y = 2;
     let z: i32 = 424234;
+    let face = '\u{1F970}';
      
 
 println!( "Max of i32 is {}",std::i32::MAX 
@@ -11,4 +12,9 @@ println!( "Max of i32 is {}",std::i32::MAX
 println!( "Max of i64 is {}",std::i64::MAX 
 
 );
+
+//Boolean
+let is_greater: bool = 10>5 ;
+
+println!("{:?}",(x,y,z,is_greater,face));
 }
