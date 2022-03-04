d8790bf73c6a3d4e830cb814371b528de001fcd3
coderasha <ashadipta@gmail.com>
2022-03-04 23:32:09 +0530

    push in RUST

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod print;
 mod vars;
 mod types;
+mod string;
 
 
 fn main() {
     print::run();
     vars::varun(); 
     types::typrun();
+    string::str();
 }
---
+++ src/string.rs
@@ -0,0 +1,18 @@
+pub fn str() {
+
+    let mut hello = String::from("Hello World");
+
+     let welcome = String::from("Welcome to the future");
+let a:i32 = 435;
+let b:i32 = 984;
+println!(
+    
+    "sum={}", a+b);
+
+
+
+hello.push_str(" with Ashadipta");
+println!("{}", hello);
+
+
+}
