b75280a0df4c84e423be3289a34220a5e36e7cd9
coderasha <ashadipta@gmail.com>
2022-07-22 00:17:03 +0530

    new

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,8 @@
 fn main(){
     let tup1 = (20,21,22);
     let tup2 =(2, true, 6.5);
+    let tup3 =(1, 2 ,(7,8,9));
     println!("the value at 2 for tup1 is {} and tup2 is {}",tup1.2, tup2.1);
+    println!("the value at 3 for tup3 is {} ",(tup3.2).2);
+
    }
