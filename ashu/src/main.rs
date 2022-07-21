f1cd1b3a1cce30d7fa577c170f10a10a0f84aa9d
coderasha <ashadipta@gmail.com>
2022-07-21 23:54:13 +0530

    new

--- ashu/src/main.rs
+++ ashu/src/main.rs
@@ -3,20 +3,34 @@
 fn main(){
 let mut n = 0;
 
-loop{
-n+=1;
 
-if n==7{
+while n<50{
 
-    return();
-    // break;
+    n+=1;
+   if n==20{
+       continue;
+   }
+    if n%5==0 {
+        println!("{}", n);
+    }
+ 
 }
-if  n==3{
-    continue;
-}
-println!("the number is {}",n);
 
-}
+
+// loop{
+// n+=1;
+
+// if n==7{
+
+//     return();
+//     // break;
+// }
+// if  n==3{
+//     continue;
+// }
+// println!("the number is {}",n);
+
+// }
 
 
 }
