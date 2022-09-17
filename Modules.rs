d62adfd338c5620a68426c4e2d4f0d16fc1b6fef
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2022-09-17 18:48:55 +0530

    Update Modules.rs

--- Modules.rs
+++ Modules.rs
@@ -24,5 +24,25 @@ movie::mov_name();
 
 }
 
+//Nested Module
+
+mod movie{
+  pub mod mobie_2{
+    
+    pub fn mov_name(){
+     println!("RRR");
+      }
+  pub fn mov_name1(){
+     println!("Bramhastra");
+      }
+  }
+  
+}
+fn main(){
+movie::movie_2::mov_name();
+  movie::movie_2::mov_name1();
+
+}
+
 
 
