886b01af5de7fd200ba68de690c00f6a7f6fddb6
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2022-09-18 03:41:37 +0530

    Update Concurrency.rs

--- Concurrency.rs
+++ Concurrency.rs
@@ -14,3 +14,29 @@ fn main (){
   
 
 }
+
+
+// Using Join Handle
+
+use std::thread;
+use std::time::Duration;
+fn main (){
+
+  let handle =  thread::spawn(||{
+
+    for mut i in 0..=10{
+        println!("{} for thread",i  );
+        thread::sleep(Duration::from_millis(5))
+    };
+});
+    for mut j in 0..=6{
+        println!("{} is printed for main",j  );
+        thread::sleep(Duration::from_millis(5))
+
+    };
+
+    handle.join().unwrap();
+
+    
+   
+}
