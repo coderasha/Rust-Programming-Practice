fe62c0326d8e0a2ba75194a3672404ae44c4fd0e
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2022-09-17 19:10:19 +0530

    Update Concurrency.rs

--- Concurrency.rs
+++ Concurrency.rs
@@ -3,5 +3,14 @@ use std::time::Duration;
 
 fn main (){
 
+  
+  thread::spawn(||{
+    for i in 1..=10{
+    println!("the no. is {:?}",i);
+      thread::sleep(Duration::millis(1)
+    };
+    }
+  );
+  
 
 }
