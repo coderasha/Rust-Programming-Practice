c54fef7cd67a317f29e2d7e0793d7fb33631181b
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2022-09-18 04:16:56 +0530

    Update Enum.js

--- Enum.js
+++ Enum.js
@@ -1,1 +1,18 @@
+#[derive(Debug)]
+enum Gender{
+
+    Male,
+    Female
+}
+fn main(){
+
+    let x = Gender::Male;
+    let y = Gender::Female;
+
+    
+    println!("{:?}",y);
+    println!("{:?}",x);
+
+
+}
 
