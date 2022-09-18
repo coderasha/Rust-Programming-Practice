0c1fdb6fd69f7759f7604e9f3146e5549974f43a
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2022-09-18 17:57:51 +0530

    Update Random.rs

--- Random.rs
+++ Random.rs
@@ -10,3 +10,12 @@ let mut number  = rnad::random::<f64>();
   in Cargo.toml insert rand = "0,8.4"
 
 ------------------------------------------------------
+
+for a range in Random number
+
+use rand::{thread_rng,Rng};
+fn main(){
+
+    let mut number = rand::thread_rng().gen_range(0..11);
+    println!("{:?}", number);
+}
